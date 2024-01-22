ab11f52562eda23dd4b15f6784de3d5eb4c91d71
Susruthad <susruthanewid@gmail.com>
2024-01-21 19:01:55 -0800

    Step buttton Completed

--- Exercise2/ViewController.swift
+++ Exercise2/ViewController.swift
@@ -11,16 +11,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelValue: UILabel!
     var tempNumberHolder = 0
+    var step = 1
     
     @IBAction func buttonInc(_ sender: Any) {
-        tempNumberHolder += 1
+        tempNumberHolder += step
         labelValue.text = String(tempNumberHolder)
     }
     
     @IBAction func buttonDec(_ sender: Any) {
-        tempNumberHolder -= 1
+        tempNumberHolder -= step
         labelValue.text = String(tempNumberHolder)
         
+    }
+    
+    @IBAction func buttonStep(_ sender: Any) {
+
+        step = 2
+    }
+    @IBAction func buttonreset(_ sender: Any) {
+        tempNumberHolder = 0
+        step = 1
+        labelValue.text = String(tempNumberHolder)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
